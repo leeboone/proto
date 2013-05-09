d1d3e75c0e1882e5b3f21e3c987db9caf3019e42
Lee Boone <lee@leeboone.com>
2013-05-09 11:46:42 -0400

    Changed to new WordPress commenting
    Better compatibility with WordPress.com & Jetpack

--- comments.php
+++ comments.php
@@ -26,31 +26,7 @@
     <?php endif; ?>   
       
     <?php if(comments_open()) : ?>  
-        <?php if(get_option('comment_registration') && !$user_ID) : ?>  
-            <p>You must be <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?redirect_to=<?php echo urlencode(get_permalink()); ?>">logged in</a> to post a comment.</p><?php else : ?>  
-            <form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
-                <fieldset>
-                    <legend>Submit your comment:</legend>
-                    <?php if($user_ID) : ?>  
-                        <p>Logged in as <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?action=logout" title="Log out of this account">Log out &raquo;</a></p>  
-                    <?php else : ?>  
-                        <div class="rowElem"><label for="author"><small>Name <?php if($req) echo "(required)"; ?></small></label><input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" size="22" tabindex="1" />  
-                        </div>  
-                        <div class="rowElem"><label for="email"><small>Mail (will not be published) <?php if($req) echo "(required)"; ?></small></label><input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" size="22" tabindex="2" />  
-                        </div>  
-                        <div class="rowElem"><label for="url"><small>Website</small></label><input type="text" name="url" id="url" value="<?php echo $comment_author_url; ?>" size="22" tabindex="3" />  
-                        </div>  
-                    <?php endif; ?>  
-                    <div class="rowElem">
-                        <h5>Allowed Tags:</h5>
-                        <pre><?php echo allowed_tags(); ?> </pre>
-                        <textarea name="comment" id="comment" cols="100%" rows="10" tabindex="4"></textarea></div>  
-                    <div class=-"rowElem"><input name="submit" type="submit" id="submit" class="btn" tabindex="5" value="Submit Comment" />  
-                    <input type="hidden" name="comment_post_ID" value="<?php echo $id; ?>" /></div>  
-                    <?php do_action('comment_form', $post->ID); ?>
-                </fieldset>  
-            </form>  
-        <?php endif; ?>  
+    <?php comment_form(); ?>
     <?php elseif($comments) : ?>  
         <p>The comments are closed.</p>  
     <?php endif; ?>    
